37391c5e595b975177b80f3d8bf33522c4ada252
bakhtiyar-k <subetegaf@mail.ru>
2023-10-19 12:39:06 +0600

    HW_5

---
+++ HW_5/predict_credit_model1.py
@@ -0,0 +1,25 @@
+import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+with open("model1.bin", "rb") as f_model:
+    model = pickle.load(f_model)
+
+with open("dv.bin", "rb") as f_dv:
+    dv = pickle.load(f_dv)
+    
+app = Flask("Bank Credit Score")
+@app.route('/predict_credit_model1', methods=["POST"])
+
+def predict_credit_model1():
+    client = request.get_json()
+    X = dv.transform(client)
+    y_pred = model.predict_proba(X)[0, 1]
+    
+    result = {"probability of getting a credit": float(y_pred)}
+    
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9696)
